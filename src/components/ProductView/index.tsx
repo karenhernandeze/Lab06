5493b23d7cc57ffd7859542423c2d41e04872553
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-09-28 23:54:37 -0600

    Challenge 2 and 3

---
+++ src/components/ProductView/index.tsx
@@ -0,0 +1,72 @@
+import "./index.css";
+import Product from "../../types/Product";
+import { Grid, Paper, Typography } from "@material-ui/core";
+
+interface ProductViewProps {
+  product: Product;
+}
+
+/**
+ * Product preview elements
+ * @returns ProductView UI elements
+ */
+const ProductView: React.FC<ProductViewProps> = (props) => {
+  var listPrice = 0.0;
+  if (
+    props.product !== undefined &&
+    props.product.childSkus !== undefined &&
+    props.product.childSkus[0] !== undefined
+  ) {
+    listPrice = props.product.childSkus[0].listPrice;
+  }
+
+  var salePrice = 0.0;
+  if (
+    props.product !== undefined &&
+    props.product.childSkus !== undefined &&
+    props.product.childSkus[0] !== undefined
+  ) {
+    salePrice = props.product.childSkus[0].salePrice;
+  }
+
+  var mediumImageUrl = "";
+  if (
+    props.product !== undefined &&
+    props.product.childSkus !== undefined &&
+    props.product.childSkus[0] !== undefined
+  ) {
+    mediumImageUrl = props.product.childSkus[0].mediumImageUrl;
+  }
+
+  return (
+    <div className="productPreview">
+      <Grid container className="productGrid" spacing={2}>
+        <Grid item lg={2}>
+          <Paper className="largeImage">
+            <img src={mediumImageUrl} alt={props.product.name} />
+          </Paper>
+        </Grid>
+        <Grid item lg={10} container>
+          <Grid item lg={12}>
+            <Typography className="productName" variant="h1">
+              {props.product.name}
+            </Typography>
+          </Grid>
+          <Grid item lg={12}>
+            <Typography className="description">
+              {props.product.description}
+            </Typography>
+          </Grid>
+          <Grid item lg={2}>
+            <Typography className="dollars crossedout">{listPrice}</Typography>
+          </Grid>
+          <Grid item lg={2}>
+            <Typography className="dollars">{salePrice}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ProductView;
--- src/containers/ProductList/index.tsx
+++ src/containers/ProductList/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import ProductView from "../../components/ProductView";
 import Product from "../../types/Product";
+import ProductService from "../../services/ProductService";
 
 interface ProductListState {
   products: Product[];
@@ -14,12 +16,36 @@ class ProductList extends React.Component<{}, ProductListState> {
     products: [] as Product[],
   };
 
+
+  componentDidMount(){
+    this.retrieveproducts = this.retrieveproducts.bind(this)
+    this.retrieveproducts()
+  }
+
+  async retrieveproducts(){
+    await ProductService.getAll().then(
+      response => {
+        console.log(response)
+        const products = response.data;
+        this.setState( {products});
+      }
+    ).catch(
+      error => {
+        console.log(error)
+      }
+    )
+  }
   /**
    * Renders the container.
    * @return {string} - HTML markup for the container
    */
   render() {
-    return <div> Products Here </div> ;
+    var productsList: any[] = [];
+    this.state.products.forEach((product) => {
+      productsList.push(<ProductView product={product} />);
+    });
+    return productsList;
+    
   }
 
 }
