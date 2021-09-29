5e01ee0eb5635d0852141c82479e9558f403a335
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-09-29 00:02:46 -0600

    Challenge 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from "./containers/Header";
 import ProductList from "./containers/ProductList";
 
+const pathname = window.location.href  
+  console.log(pathname)
+const localHost = "http://localhost:3000/"
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Header/>
-      {/* <ProductDetail/> */}
-      <ProductList/>
+      {
+        pathname === localHost ? <ProductList/> : <ProductDetail/>
+      }
     </React.Fragment>
   );
 }
