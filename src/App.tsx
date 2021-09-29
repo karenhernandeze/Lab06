9b2c2f97e652b00330af5a1f47a389cf6e4b2a28
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-09-28 23:56:22 -0600

    Challenge 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import ProductDetail from './containers/ProductDetail';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from "./containers/Header";
+import ProductList from "./containers/ProductList";
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Header/>
-      <ProductDetail/>
+      {/* <ProductDetail/> */}
+      <ProductList/>
     </React.Fragment>
   );
 }
